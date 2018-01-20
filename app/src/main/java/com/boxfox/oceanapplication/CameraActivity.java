f5c0f564aadf6b117cb9cd6c182ca1c4a8ae5738
rlatjdfo112 <rlatjdfo112@naver.com>
2018-01-20 19:26:49 +0900

    Implement the camera capture & layout

---
+++ app/src/main/java/com/boxfox/oceanapplication/CameraActivity.java
@@ -0,0 +1,191 @@
+package com.boxfox.oceanapplication;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.buffaloes.oceanapplication.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class CameraActivity extends Activity {
+    private SurfaceView preview;
+    private SurfaceHolder previewHolder;
+    private Camera camera;
+
+    private ProgressDialog progressDialog;
+
+    private boolean inPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        preview = (SurfaceView) findViewById(R.id.surface_camera_preview);
+
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        previewHolder.setFixedSize(preview.getWidth(), preview.getHeight());
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+        return (result);
+    }
+
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            } catch (Throwable t) {
+                Log.e("PreviewDemo", "Exception in setPreviewDisplay()", t);
+                Toast.makeText(CameraActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        public void surfaceChanged(SurfaceHolder holder,
+                                   int format, int width,
+                                   int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            Camera.Size size = getBestPreviewSize(width, height,
+                    parameters);
+
+            if (size != null) {
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                inPreview = true;
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // no-op
+        }
+    };
+
+
+    Camera.PictureCallback photoCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(final byte[] data, final Camera camera) {
+            progressDialog = ProgressDialog.show(CameraActivity.this, "", "Saving Photo");
+            new Thread() {
+                public void run() {
+                    onPictureTake(data, camera);
+                }
+            }.start();
+        }
+    };
+
+
+    public void onPictureTake(byte[] data, Camera camera) {
+        Log.d("Test", "asdsad");
+        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+        Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        savePhoto(mutableBitmap);
+        progressDialog.dismiss();
+    }
+
+
+    class SavePhotoTask extends AsyncTask<byte[], String, String> {
+        @Override
+        protected String doInBackground(byte[]... jpeg) {
+            File photo = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+            if (photo.exists()) {
+                photo.delete();
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(photo.getPath());
+                fos.write(jpeg[0]);
+                fos.close();
+            } catch (java.io.IOException e) {
+                Log.e("PictureDemo", "Exception in photoCallback", e);
+            }
+            return (null);
+        }
+    }
+
+
+    public void savePhoto(Bitmap bmp) {
+        /*FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(imageFileName);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            out = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public String fromInt(int val) {
+        return String.valueOf(val);
+    }
+
+    public void onBack() {
+        inPreview = false;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        camera = Camera.open();
+    }
+
+    @Override
+    public void onPause() {
+        if (inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+        super.onPause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                camera.takePicture(null, null, photoCallback);
+                return false;
+        }
+
+        if (keyCode == KeyEvent.KEYCODE_MENU && event.getRepeatCount() == 0) {
+            onBack();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
--- app/src/main/java/com/boxfox/oceanapplication/SplashActivity.java
+++ app/src/main/java/com/boxfox/oceanapplication/SplashActivity.java
@@ -1,5 +1,14 @@
 package com.boxfox.oceanapplication;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,12 +24,14 @@ import com.facebook.login.widget.LoginButton;
 import io.realm.Realm;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 200;
     private LoginButton login_facebook;
     private View login_facebook_fake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getPermission();
         FBLoginUtil.print_facebook_keyhash(this);
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
@@ -29,6 +40,48 @@ public class SplashActivity extends AppCompatActivity {
         login_facebook = findViewById(R.id.btn_splash_facebook_login);
         checkUserData();
         initLoginButton();
+    }
+
+    private void getPermission() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.NFC) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                }
+            } else {
+                next();
+            }
+        } else {
+            next();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                next();
+            } else {
+                AlertDialog.Builder ab = new AlertDialog.Builder(SplashActivity.this);
+                ab.setMessage("펴미션을 허용하지 않으면 Schooler를 사용하실 수 없습니다!");
+                ab.setPositiveButton("확인", null);
+                ab.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+                ab.show();
+            }
+        }
+    }
+
+    private void next() {
+        startActivity(new Intent(this, CameraActivity.class));
     }
 
     private void checkUserData() {
